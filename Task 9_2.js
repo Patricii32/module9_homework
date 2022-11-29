ef44bf9e1ff8300f03bbdad11280783226c67ae8
Denis Kravtsov <kravmag@yandex.ru>
2022-11-29 14:31:06 +0300

    Update Task 9_2.js

---
+++ Task 9_2.js
@@ -0,0 +1,58 @@
+/*Задание 2.
+Вам дана заготовка и результат, который вы должны получить. Ваша задача — написать код, который будет преобразовывать JSON в JS-объект и выводить его в консоль.
+
+JSON:
+{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+JS-объект:
+
+{
+  list: [
+    { name: 'Petr', age: 20, prof: 'mechanic' },
+    { name: 'Vova', age: 60, prof: 'pilot' },
+  ]
+}
+*/
+
+const jsonString =`
+{
+ "list": [
+  {
+   "name": "Petr",
+   "age": "20",
+   "prof": "mechanic"
+  },
+  {
+   "name": "Vova",
+   "age": "60",
+   "prof": "pilot"
+  }
+ ]
+}
+`;
+const arr = [];
+const data = JSON.parse(jsonString);
+const list = data.list;
+
+list.forEach(list =>{
+  let person ={
+    name: list.name,
+    age: list.age,
+    prof: list.prof,
+   }
+  arr.push(person);
+});
+
+console.log(arr);
